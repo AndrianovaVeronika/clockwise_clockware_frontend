832b8ca48afe83d375e2baed6f3aacb4d732752a
AndrianovaVeronika <89983879+AndrianovaVeronika@users.noreply.github.com>
2022-01-16 21:58:29 +0200

    fix: .env initial

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
+require('dotenv').config();
 
 ReactDOM.render(
     <BrowserRouter>
--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.DB_LINK + ':' + process.env.DB_PORT,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
