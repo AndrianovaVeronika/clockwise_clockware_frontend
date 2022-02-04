dc6ee7eeca5daa4f6d3671cf825e18c0a0e8e87c
AndrianovaVeronika <89983879+AndrianovaVeronika@users.noreply.github.com>
2022-02-04 22:03:54 +0200

    fix: .env

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.REACT_APP_BASE_URL_CONNECT,
     // baseURL: 'http://' + process.env.DB_HOST + ':' + process.env.DB_PORT,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
